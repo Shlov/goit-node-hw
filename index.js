9766ba6b110810edc3c85a8749812b9a73e933d9
Anton Shabalov <shabalovanton@gmail.com>
2023-03-31 21:38:54 +0300

    add ignore

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const {listContacts, getContactById, removeContact, addContact} =require('./contacts.js');
+const { Command } = require("commander");
 
 // test fnc
 
@@ -6,6 +7,48 @@ const {listContacts, getContactById, removeContact, addContact} =require('./cont
 // getContactById('AeHIrLTr6JkxGE6SN-0Rw');
 // removeContact('qdggE76Jtbfd9eWJHrssH');
 // addContact("Carl Morz", "morz@egetlacus.ca", "(295) 532-6812");
+
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// TODO: рефакторить
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      // ...
+      listContacts()
+      break;
+
+    case "get":
+      // ... id
+      getContactById(id);
+      break;
+
+    case "add":
+      // ... name email phone
+      addContact(name, email, phone)
+      break;
+
+    case "remove":
+      // ... id
+      removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
 
 console.log('Hello!')
 
